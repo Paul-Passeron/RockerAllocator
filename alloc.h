bcda16b23a1061b36af3258d9275a582d23d5787
Paul Passeron <paul.passeron2@gmail.com>
2024-02-25 19:54:13 +0100

    format changes

--- alloc.h
+++ alloc.h
@@ -10,13 +10,13 @@
 #include <stdlib.h>
 
 typedef struct alloc_elem_t {
-  void *ptr;
+  void* ptr;
   int scope;
 } alloc_elem_t;
 
 typedef struct alloc_stack_t {
-  alloc_elem_t *data;
-  void **persistents;
+  alloc_elem_t* data;
+  void** persistents;
   size_t capacity;
   size_t capacity_p;
   int length;
@@ -24,17 +24,17 @@ typedef struct alloc_stack_t {
   size_t scope;
 } alloc_stack_t;
 
-void init_stack(alloc_stack_t *alloc);
+void init_stack(alloc_stack_t* alloc);
 void kill_stack(alloc_stack_t alloc);
 
-void new_scope(alloc_stack_t *alloc);
-void end_scope(alloc_stack_t *alloc);
+void new_scope(alloc_stack_t* alloc);
+void end_scope(alloc_stack_t* alloc);
 
-void *allocate(alloc_stack_t *alloc, size_t size);
-void *allocate_persistent(alloc_stack_t *alloc, size_t size);
+void* allocate(alloc_stack_t* alloc, size_t size);
+void* allocate_persistent(alloc_stack_t* alloc, size_t size);
 
-void *reallocate(alloc_stack_t *alloc, void *ptr, size_t size);
-void *reallocate_persistent(alloc_stack_t *alloc, void *ptr, size_t size);
+void* reallocate(alloc_stack_t* alloc, void* ptr, size_t size);
+void* reallocate_persistent(alloc_stack_t* alloc, void* ptr, size_t size);
 
 void init_compiler_stack(void);
 
@@ -44,12 +44,12 @@ void new_compiler_scope(void);
 
 void end_compiler_scope(void);
 
-void *allocate_compiler(size_t size);
+void* allocate_compiler(size_t size);
 
-void *allocate_compiler_persistent(size_t size);
+void* allocate_compiler_persistent(size_t size);
 
-void *reallocate_compiler(void *ptr, size_t size);
+void* reallocate_compiler(void* ptr, size_t size);
 
-void *reallocate_compiler_persistent(void *ptr, size_t size);
+void* reallocate_compiler_persistent(void* ptr, size_t size);
 
-#endif // ALLOC_H
+#endif  // ALLOC_H
